8f044356f3ee1ebb6037457bbb5824470362974e
Richard Kneale <richard@mantaraymedia.co.uk>
2018-11-30 22:07:41 +0000

    Updated store

--- website-vue/src/store/index.ts
+++ website-vue/src/store/index.ts
@@ -1,17 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import IArt from "../interfaces/IArt";
-
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        art: []
     },
     mutations: {
-        setArt(state: any, art: IArt): void {
-            state.art = art;
-        }
     }
 });
