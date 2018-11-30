4e86cb656073bf729a5d979e5225d20244018958
Richard Kneale <richard@mantaraymedia.co.uk>
2018-11-30 21:16:34 +0000

    Updated art model

--- website-vue/src/models/ArtModel.ts
+++ website-vue/src/models/ArtModel.ts
@@ -38,8 +38,209 @@ export default class ArtModel {
                     file: "pyramid_model.png"
                 },
                 {
-                    name: "Untitled",
-                    file: "untitled.png"
+                    name: "Ta Prohm",
+                    file: "ta_prohm.png"
+                }
+            ]
+        },
+        {
+            directory: "miscellaneous",
+            images: [
+                {
+                    name: "Gaige (Borderlands 2)",
+                    file: "gaige.png"
+                },
+                {
+                    name: "Gaige (Lines)",
+                    file: "gaige_lines.png"
+                },
+                {
+                    name: "Double Header",
+                    file: "double_header.png"
+                }
+            ]
+        },
+        {
+            directory: "tekken",
+            images: [
+                {
+                    name: "Dragon Uppercut",
+                    file: "dragon_uppercut.png"
+                },
+                {
+                    name: "Christie Monteiro",
+                    file: "christie_monteiro.png"
+                },
+                {
+                    name: "Jin Kazama",
+                    file: "jin_kazama.png"
+                },
+                {
+                    name: "Hwoarang",
+                    file: "hwoarang.png"
+                },
+                {
+                    name: "Julia Chang",
+                    file: "julia_chang.png"
+                }
+            ]
+        },
+        {
+            directory: "panic_attack",
+            images: [
+                {
+                    name: "Panic Attack 3D Model",
+                    file: "panic_attack.png"
+                },
+                {
+                    name: "Panic Attack (Work in Progress)",
+                    file: "panic_attack_wip.png"
+                },
+                {
+                    name: "Panic Attack Wallpaper",
+                    file: "panic_attack_wallpaper.png"
+                }
+            ]
+        },
+        {
+            directory: "mass_effect",
+            images: [
+                {
+                    name: "Sara Ryder",
+                    file: "sara_ryder.png"
+                },
+                {
+                    name: "Mordin Solus",
+                    file: "mordin_solus.png"
+                },
+                {
+                    name: "Mordin Solus (Lines)",
+                    file: "mordin_lines.png"
+                },
+                {
+                    name: "Tali'Zorah nar Rayya",
+                    file: "tali_zorah.png"
+                }
+            ]
+        },
+        {
+            directory: "concepts",
+            images: [
+                {
+                    name: "The Ripper",
+                    file: "ripper.png"
+                },
+                {
+                    name: "Demoralator",
+                    file: "demoralator.png"
+                },
+                {
+                    name: "Demoralator (Lines)",
+                    file: "demoralator_lines.png"
+                },
+                {
+                    name: "The Twins",
+                    file: "twins.png"
+                },
+                {
+                    name: "Twin",
+                    file: "twin.png"
+                }
+            ]
+        },
+        {
+            directory: "resident_evil",
+            images: [
+                {
+                    name: "Rebecca Chambers",
+                    file: "rebecca_chambers.png"
+                },
+                {
+                    name: "Claire & Chris Redfield",
+                    file: "claire_chris.png"
+                },
+                {
+                    name: "Resident Evil",
+                    file: "resident_evil.png"
+                },
+                {
+                    name: "Resident Evil (Lines)",
+                    file: "resident_evil_lines.png"
+                },
+                {
+                    name: "Pluto",
+                    file: "pluto.png"
+                }
+            ]
+        },
+        {
+            directory: "toys",
+            images: [
+                {
+                    name: "Undertaker Plush Toy",
+                    file: "undertaker.png"
+                },
+                {
+                    name: "Kane Plush Toy",
+                    file: "kane.png"
+                },
+                {
+                    name: "Bret Hart Plush Toy",
+                    file: "bret_hart.png"
+                },
+                {
+                    name: "Don West Plush Toy",
+                    file: "don_west.png"
+                },
+                {
+                    name: "Jaycee Custom Action Figure",
+                    file: "jaycee.png"
+                },
+                {
+                    name: "Jaycee Custom Action Figure",
+                    file: "king.png"
+                }
+            ]
+        },
+        {
+            directory: "hammer_troll",
+            images: [
+                {
+                    name: "Hammer Troll",
+                    file: "hammer_troll.png"
+                },
+                {
+                    name: "Hammer Troll 3D Model",
+                    file: "troll_model.png"
+                },
+                {
+                    name: "Hammer Troll Sketch",
+                    file: "troll_sketch.png"
+                }
+            ]
+        },
+        {
+            directory: "wrestling",
+            images: [
+                {
+                    name: "Wrestler Concepts",
+                    file: "wrestler_concepts_001.png"
+                },
+                {
+                    name: "Wrestler Concepts",
+                    file: "wrestler_concepts_002.png"
+                },
+                {
+                    name: "Wrestler 3D Model",
+                    file: "wrestler_model.png"
+                },
+                {
+                    name: "Wrestler Concept",
+                    file: "wrestler_concept.png"
+                },
+                {
+                    name: "Alley Entrance Concept",
+                    file: "alley_entrance.png"
                 }
             ]
         }
